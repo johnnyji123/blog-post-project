b30ead65e9984db7b2af9544bc9f736e978f9c08
Johnny Ji <jj_johnny97@hotmail.com>
2023-12-26 09:38:43 +0000

    created post_blog.html with boilerplate html

--- temp.py
+++ temp.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 
 
@@ -20,8 +20,30 @@ cursor = db.cursor()
 
 app = Flask(__name__)
 
-def
 
+def render_dictionary(query):
+    col_names = [name[0] for name in cursor.description]
+    data_lst = []
+    
+    all_rows = cursor.fetchall()
+    
+    for row in all_rows:
+        row_dict = {}
+        for col_name, value in zip(col_names, row):
+            row_dict[col_name] = value
+        data_lst.append(row_dict)
+    
+    return data_lst
+
+    
+    
+    
+    
+
+@app.route("/post_blog", methods = ["GET", "POST"])
+def post_blog():
+    
+    return render_template("post_blog.html")
 
 
 if __name__ == ("__main__"):
