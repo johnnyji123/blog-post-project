134d82a7d86483fc18115de798396702d8dc508f
Johnny Ji <jj_johnny97@hotmail.com>
2023-12-26 12:34:05 +0000

    saved images as filepath

--- temp.py
+++ temp.py
@@ -3,8 +3,8 @@ from flask import Flask, url_for, request, render_template
 import random
 import uuid
 from datetime import date
-
-
+import sys
+import os
 
 db = mysql.connector.connect(
         host = "localhost",
@@ -21,7 +21,7 @@ cursor = db.cursor()
 
 
 app = Flask(__name__)
-
+app.config['images'] = r'C:\Users\jj_jo\blog-post-project\Flask_app\images'
 
 def render_dictionary(query):
     col_names = [name[0] for name in cursor.description]
@@ -40,27 +40,34 @@ def render_dictionary(query):
     
     
     
-    
 
 @app.route("/post_blog", methods = ["GET", "POST"])
 def post_blog():
     if request.method == "POST":
         title = request.form.get("post_title")
         content = request.form.get("post_content")
-        image = request.form.get("featured_image")
         post_id = uuid.uuid4()
         post_id_str = str(post_id)
         author_id = uuid.uuid4()
         author_id_str = str(author_id)
         publication_date = date.today()
         
+        
+        if 'filename' in request.files:
+            file = request.files['filename']
+            
+            if file:
+                file_path = os.path.join(app.config['images'], file.filename)
+                file.save(file_path)
+            
+            
         cursor.execute(
                 """
                 INSERT INTO blog_posts
-                (post_id, post_title, post_content, author_id, publication_date, featured_image)
+                (post_id, post_title, post_content, author_id, publication_date, file_path)
                 VALUES (%s, %s, %s, %s, %s, %s)
                 """,
-                (post_id_str, title, content, author_id_str, publication_date, image)
+                (post_id_str, title, content, author_id_str, publication_date, file_path)
             )                    
         
         db.commit()
@@ -71,10 +78,19 @@ def post_blog():
 
 @app.route("/", methods = ["GET", "POST"])
 def show_blogs():
-    query = cursor.execute("SELECT post_title, post_content, publication_date, featured_image FROM blog_posts")
+    query = cursor.execute("SELECT post_title, post_content, publication_date FROM blog_posts")
     blog_data = render_dictionary(query)
+    
+
     
     return render_template("home_page.html", blog_data = blog_data)
 
+
+
+
+
 if __name__ == ("__main__"):
     app.run(debug = True, use_reloader = False)
+
+
+
